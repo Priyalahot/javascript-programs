28252933fa7f2c5266aed33240c256bebff32914
Priya Lahot <priyalah1234@gmail.com>
2022-07-18 11:33:23 +0530

    jul 18 code

---
+++ 18-07-22/include.js
@@ -0,0 +1,11 @@
+
+        let msg = "hello saurabh";
+        let num =10;
+
+        function multiplyNum(a,b){
+            return a * b;
+        }
+
+        // exporting variable and function
+
+        export { msg, num , multiplyNum};
---
+++ 18-07-22/main.js
@@ -0,0 +1,7 @@
+import { msg, num, multiplyNum } from "./include.js";
+device-width
+console.log(msg);
+
+console.log(num);
+
+console.log(multiplyNum(2,3));
